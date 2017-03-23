9e67c9574c73b399c91aa0dbe02e11a12d116de3
Srini Seetharaman <seethara@stanford.edu>
2017-03-23 14:28:12 -0700

    modify mn_confige

--- mininet/mn_confige.py
+++ mininet/mn_confige.py
@@ -3,22 +3,26 @@ This file includes the configuration of th topology and controller of
 the mininet which used to create the test environment.
 '''
 
-from mininet.topo import Topo
+from mininet.topo import Topo 
+from mininet.node import RemoteController, OVSSwitch
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.cli import CLI
 
 class TestTopo(Topo):
 
     def __init__(self):
 
-        Topo.__init__(self):
+        Topo.__init__(self)
 
         # Add hosts and switches
-        h00 = self.addHost('h00')
-        h01 = self.addHost('h01')
-        s0  = self.addSwitch('s0')
+        h00 = self.addHost('h00', ip='10.0.0.2', mac='00:00:00:00:00:01')
+        h01 = self.addHost('h01', ip='10.0.0.3', mac='00:00:00:00:00:02')
+        s0  = self.addSwitch('s0', ip='10.0.0.1', mac='00:00:00:00:00:03')
 
-        h10 = self.addHost('h10')
-        h11 = self.addHost('h11')
-        s1  = self.addSwitch('s1')
+        h10 = self.addHost('h10', ip='10.0.1.2', mac='00:00:00:00:00:04')
+        h11 = self.addHost('h11', ip='10.0.1.3', mac='00:00:00:00:00:05')
+        s1  = self.addSwitch('s1', ip='10.0.1.1',mac='00:00:00:00:00:06')
 
         # Add links
         self.addLink(h00, s0)
@@ -27,5 +31,16 @@ class TestTopo(Topo):
         self.addLink(h11, s1)
         self.addLink(s0, s1)
 
+def basicTest():
+    c0 = RemoteController('c0', ip='127.0.0.1', port=6633)
+    net = Mininet(topo=TestTopo(),controller=None, switch=OVSSwitch)
+    net.addController(c0)
+    net.start()
+    net.pingAll()
+    CLI(net)
+    net.stop()
 
-topos = {'testtopo' : (lambda: TestTopo())}
+if __name__ == '__main__':
+    setLogLevel('info')
+    basicTest()
+#topos = {'testtopo' : (lambda: TestTopo())}
--- mininet/mn_oprate.py
+++ mininet/mn_oprate.py
@@ -1,3 +1,5 @@
 '''
 This file includes the  opration commands of the mininet
 '''
+Open mininet CLI with test topology:
+    sudo mn --custom ./mn_confige.py --topo testtopo
