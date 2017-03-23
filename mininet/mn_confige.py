7c216f11106f70553b3101f31a1840a4daf258ae
Srini Seetharaman <seethara@stanford.edu>
2017-03-22 21:00:32 -0700

    add topology configuration

--- mininet/mn_confige.py
+++ mininet/mn_confige.py
@@ -2,3 +2,30 @@
 This file includes the configuration of th topology and controller of
 the mininet which used to create the test environment.
 '''
+
+from mininet.topo import Topo
+
+class TestTopo(Topo):
+
+    def __init__(self):
+
+        Topo.__init__(self):
+
+        # Add hosts and switches
+        h00 = self.addHost('h00')
+        h01 = self.addHost('h01')
+        s0  = self.addSwitch('s0')
+
+        h10 = self.addHost('h10')
+        h11 = self.addHost('h11')
+        s1  = self.addSwitch('s1')
+
+        # Add links
+        self.addLink(h00, s0)
+        self.addLink(h01, s0)
+        self.addLink(h10, s1)
+        self.addLink(h11, s1)
+        self.addLink(s0, s1)
+
+
+topos = {'testtopo' : (lambda: TestTopo())}
