7fc20964c4f2805263d4720c1a63e09e18ece74f
jinrui wang <nideyangzi1989@163.com>
2017-04-13 17:22:24 -0400

    update auth

--- firewall/server/request_handler.py
+++ firewall/server/request_handler.py
@@ -1,3 +1,5 @@
+import logging
+
 import json
 import requests
 from tornado.web import RequestHandler
@@ -8,19 +10,35 @@ from requests.auth import HTTPBasicAuth
 import odl_rest_url
 
 
+logger = logging.getLogger('server')
+FORMAT = '%(asctime)s - %(filename)s - %(funcName)s - %(lineno)d - %(levelname)s - %(module)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+
 class IndexHandler(RequestHandler):
+    def initialize(self):
+        logger.info(
+            'Receive request from %s',
+            self.request.remote_ip
+        )
+
     def get(self):
-        self.render("templete/index.html")
+        self.render("template/index.html")
 
 
 class SetACLRuleHandler(RequestHandler):
     def initialize(self):
+        logger.info(
+            'Receive request from %s',
+            self.request.remote_ip
+        )
         self.controller_ip = None
         self.controller_port = None
         self.base_auth = None
 
     def post(self):
         request_body = json_decode(self.request.body)
+        logger.info('Request body %s', request_body)
         
         if request_body:
             if request_body['post_type'] == 'aclrule':
@@ -28,12 +46,11 @@ class SetACLRuleHandler(RequestHandler):
             else:
                 self.authentication(request_body)
 
-        self.write("Set ACL Rule fail")
         return
 
     def set_acl_rule(self, body):
-        base_url = 'http://' + self.controller_ip + ':' \
-                + self.controller_port
+        base_url = 'http://' + str(self.controller_ip) + ':' \
+                + str(self.controller_port)
         url = base_url+ietf_al
 
         input_data = {}
@@ -41,18 +58,39 @@ class SetACLRuleHandler(RequestHandler):
                 auth=self.base_auth)
 
         if ret.status_code == 200:
-            rewrite(" success ")
-
+            self.write(" success ")
+        else:
+            self.write_error(500)
         return
 
     def authentication(self, body):
         try:
             self.controller_ip = body['controller_ip'] 
             self.controller_port = body['controller_port'] 
-            self.base_auth = self.body['authorization'] 
+            self.base_auth = body['authentication'] 
         except:
-           self.write("Authentication") 
+           self.write("Authentication failed!!") 
+
+        topo_api = '/restconf/operational/network-topology:network-topology'
+        topo_url = 'http://' + str(self.controller_ip) + ':' + \
+                str(self.controller_port) + topo_api
+
+        headers = {'authorization': self.base_auth}
+        input_data = {}
+        node_list = []
+        ret = requests.get(topo_url, headers=headers)
+        if ret:
+            network_topology = ret.json()['network-topology']
+            try:
+                nodes = network_topology['topology'][0]['node']
+                for node in nodes:
+                    node_list.append(node['node-id'])
+            except:
+               pass 
+
+        if ret.status_code == 200:
+            self.write(json.dumps(node_list))
+        else:
+            self.write_error(500)
+
         return
-        
-
-
